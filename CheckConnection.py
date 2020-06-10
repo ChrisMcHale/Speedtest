3fa33bd67af35881a3f230cdfbb36b36e554bc1d
Chris <chaostime@gmail.com>
2020-06-10 12:53:21 +0100

    Added comments

--- CheckConnection.py
+++ CheckConnection.py
@@ -1,13 +1,16 @@
 import speedtest, DatabaseController, sys, logging, os
 
+# Initialise and configure the logger
 log = logging.getLogger(__name__)
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=os.environ.get("LOGLEVEL", "INFO"))
 
+#Declare the globals
 s = speedtest.Speedtest()
 dbc = DatabaseController.DatabaseController()
 
 
 def getSpeedTestResults():
+    #Pre-populate the results dictionary with fail-state responses
     results = {
         "server": "Unable to determine server",
         "bytes_sent": "0",
@@ -19,10 +22,13 @@ def getSpeedTestResults():
     }
     log.info("Attempting Speedtest")
     try:
+        #Attempt to find the best server to test against
         if (s.get_best_server()):
             log.info("Connected to server "+s.best["host"])
+            #If we've located a server, begin the upload and download tests
             s.download()
             s.upload(pre_allocate=False)
+            #Populate the results dictionary with real results
             results["server"] = s.best["host"]
             results["bytes_sent"] = str(s.results.bytes_sent)
             results["bytes_rec"] = str(s.results.bytes_received)
@@ -31,6 +37,10 @@ def getSpeedTestResults():
             results["ping"] = str(s.results.ping)
             results["url"] = s.results.share()
             log.info("Sending results to database")
+            #Add the measured metrics to the database
+            dbc.addResults(results)
+        else:
+            #If we can't get a server to test against, submit the fail-state results
             dbc.addResults(results)
     except:
         error = sys.exc_info()[0]
--- DatabaseController.py
+++ DatabaseController.py
@@ -10,12 +10,14 @@ class DatabaseController:
     cursor = ''
 
     def __init__(self):
+        #Get the configuration values when we're instantiated
         global config
         config = self.getConfig()
 
     def connectToDB(self):
         global db
         global cursor
+        #Attempt to connect the database using values from the configuration file
         try:
             log.info("Connecting to Database")
             db = mysql.connector.connect(
@@ -27,12 +29,14 @@ class DatabaseController:
             log.info("Connected")
         except mysql.connector.Error as error:
             log.error("Connection Error".format(error))
+        #Create a cursor so that we can execute prepared statements against the database
         cursor = db.cursor(prepared=True)
         return cursor
 
     def disconnectFromDB(self):
         global db
         global cursor
+        #Attempt a clean disconnection from the database
         try:
             log.info("Disconnecting from Database")
             if db.is_connected():
@@ -44,24 +48,33 @@ class DatabaseController:
 
     def addResults(self, results):
         global cursor
+        #Define the prepared statement we should use to enter the results into our database table
         query = """INSERT INTO results (date, sent, recieved, download, upload, ping, server, url) VALUES (%s,%s,%s,%s,%s,
         %s,%s,%s) """
+        #Create and populate a tuple containing the results, from the passed results dictionary, that we want to enter into the database
+        #In addition, prepend the tuple with the current date and time
         qtuple = (
         str(datetime.now()), results.get("bytes_sent"), results.get("bytes_rec"), results.get("download_rate"),
         results.get("upload_rate"), results.get("ping"), results.get("server"), results.get("url"))
         try:
+            #Connect to the Database
             self.connectToDB()
             log.info("Inserting values")
+            #Execute the prepared statement, loaded with the results tuple
             cursor.execute(query, qtuple)
+            #Commit the changes to the datebase
             db.commit()
             log.info(str(cursor.rowcount) + " record inserted")
+            #Disconnect cleanly
             self.disconnectFromDB()
         except mysql.connector.Error as error:
             log.exception(" Error {}".format(error))
 
     def getConfig(self):
         try:
+            #Open the database config file
             with open('database.json') as config_file:
+                #Create and populate the config object so we can access the values later
                 config = json.load(config_file)
             return config
         except IOError:
